cdf19b51b0fa9b49248744767ef65a4574711f90
Alona Enraght-Moony <code@alona.page>
2025-06-23 17:52:01 +0100

    update.sh: Allow specifying ref to pull from.
    Previously, we could only update to rust-lang/rust master. But now we
    need to publish multiple format versions, as there's a backlog that have
    landed upstream but not here.
    (https://github.com/rust-lang/rustdoc-types/issues/50).

--- update.sh
+++ update.sh
@@ -4,14 +4,26 @@ pyjq() {
     python3 -c "import json, sys; print(json.load(sys.stdin)${1})"
 }
 
+case "$#" in
+    0)
+        ref="master"
+        ;;
+    1)
+        ref="$1"
+        ;;
+    2)
+        echo "usage: ./update.sh [ref]"
+        exit 1
+        ;;
+esac
+
 user="rust-lang"
 repo="rust"
-branch="master"
 
-curl -# https://raw.githubusercontent.com/${user}/${repo}/${branch}/src/rustdoc-json-types/lib.rs \
+curl -# https://raw.githubusercontent.com/${user}/${repo}/${ref}/src/rustdoc-json-types/lib.rs \
     | sed '/^pub type FxHashMap.*$/d' \
     > src/lib.rs
 
-curl -# https://raw.githubusercontent.com/${user}/${repo}/${branch}/src/rustdoc-json-types/tests.rs > src/tests.rs
+curl -# https://raw.githubusercontent.com/${user}/${repo}/${ref}/src/rustdoc-json-types/tests.rs > src/tests.rs
 
-curl -# "https://api.github.com/repos/${user}/${repo}/commits?sha=${branch}&path=src/rustdoc-json-types/lib.rs" | pyjq '[0]["sha"]' > COMMIT.txt
+curl -# "https://api.github.com/repos/${user}/${repo}/commits?sha=${ref}&path=src/rustdoc-json-types/lib.rs" | pyjq '[0]["sha"]' > COMMIT.txt
